44de5e3e31ac3e2d09921b71d90275bb0148385c
tjsdlf99 <supercartoon@naver.com>
2018-04-04 21:15:20 +0900

    BOJ_11727

--- src/BOJ_11727.java
+++ src/BOJ_11727.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
 
-public class BOJ_11726 {
-	// BOJ_11726
-
-	static int[] dp = new int[1001];
-
+public class BOJ_11727 {
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		int n = sc.nextInt();
+		
+		int[] dp = new int[1001];
 		dp[0] = 1;
 		dp[1] = 1;
-		for (int i = 2; i <= n; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2];
+		for(int i=2;i<=n;i++) {
+			dp[i] = dp[i-2] * 3 + dp[i-1] - dp[i-2];
 			dp[i] %= 10007;
 		}
 		System.out.println(dp[n]);
-		sc.close();
 	}
-
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-	// BOJ_11726
-	
-	static int[] dp = new int[1001];
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int n = sc.nextInt();
-		dp[0] = 1;
-		dp[1] = 1;
-		for(int i=2;i<=n;i++) {
-			dp[i] = dp[i-1] + dp[i-2];
-		}
-		int result = dp[n] % 10007;
-		System.out.println(result);
-		sc.close();
 	}
 	
 }
