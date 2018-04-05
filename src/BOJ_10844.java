001a8d26a580f6e4d5306a50d45bc8a2c84ca5fe
supercartoon <supercartoon@naver.com>
2018-04-05 16:19:47 +0900

    BOJ_10844 2차원 배열로 수정

--- src/BOJ_10844.java
+++ src/BOJ_10844.java
@@ -4,13 +4,38 @@ import java.util.Scanner;
 public class BOJ_10844 {
 	
 	private static Scanner sc;
+	private static long mod = 1000000000L;
 	
 	static BigInteger[] count = new BigInteger[10];
+	
+	static long[][] d;
 
 	public static void main(String[] args) {
 		sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
+		
+		d = new long[n+1][10];
+		////
+//		18404112
+		for(int i=1;i<=9;i++) d[1][i] = 1;
+		for(int i=2;i<=n;i++) {
+			for(int j=0;j<10;j++) {
+				d[i][j] = 0;
+				if(j-1 >= 0) d[i][j] += d[i-1][j-1];
+				if(j+1 <= 9) d[i][j] += d[i-1][j+1];
+				d[i][j] %= mod;
+			}
+		}
+		
+		long sum1 = 0;
+		for(int i=0;i<10;i++) {
+			sum1 += d[n][i];
+		}
+		sum1 %= mod;
+		System.out.println(sum1);
+		
+		////
 		
 		for(int i=1;i<=9;i++) {
 			count[i] = new BigInteger("1");
@@ -42,6 +67,10 @@ public class BOJ_10844 {
 			
 			count = newArr;
 		}
+	}
+	
+	static void go2(int n) {
+		
 	}
 	
 }
